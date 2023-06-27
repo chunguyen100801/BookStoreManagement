0f5dd743a2ad349068caaf716b48fd262bbdcffa
20120443 <nguyenchuaqna@gmail.com>
2023-06-27 17:34:18 +0700

    Fix some bug

--- Source/routers/account.route.js
+++ Source/routers/account.route.js
@@ -1,22 +1,3 @@
-// const app = require('express')
-// const router = app.Router();
-
-// const accountC = require('../controllers/account.ct');
-
-// router.get('/', (req, res, next) => {
-//     res.redirect('/login')
-// });
-
-// router.get('/login', accountC.getLogin);
-// router.post('/login', accountC.postLogin);
-
-
-// router.get('/is_available', accountC.isAvailable);
-
-// router.get('/reset', accountC.getResetPw);
-// router.post('/reset', accountC.postResetPw);
-
-// module.exports = router;
 
 const app = require('express')
 const router = app.Router();
